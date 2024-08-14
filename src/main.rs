1d17222ec169ca415bdd12265f63775e7147e659
Riccardo Gallo <riccardo.gallo@secomind.com>
2024-08-14 12:42:33 +0200

    feat(deps): add deps for logging and error reporting
    Introduce:
    - `color_eyre` to have a structured report handler
    - `tracing_subscriber` to collect scoped, structured and async-aware log data
    
    Signed-off-by: Riccardo Gallo <riccardo.gallo@secomind.com>

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,20 @@
 //
 // SPDX-License-Identifier: Apache-2.0
 
-fn main() {
+use color_eyre::eyre;
+use tracing_subscriber::layer::SubscriberExt;
+use tracing_subscriber::util::SubscriberInitExt;
+use tracing_subscriber::EnvFilter;
+
+fn main() -> eyre::Result<()> {
+    color_eyre::install()?;
+
+    tracing_subscriber::registry()
+        .with(tracing_subscriber::fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .try_init()?;
+
     println!("stream-rust-test");
+
+    Ok(())
 }
