a60d71bfacb6fc68e9ac279f869cffc2070de07e
Riccardo Gallo <riccardo.gallo@secomind.com>
2024-09-27 17:37:31 +0200

    feat(shutdown): handle SIGINT/SIGTERM signals
    Gracefully shut down all the active tasks when SIGINT or SIGTERM
    signals are received.
    
    Signed-off-by: Riccardo Gallo <riccardo.gallo@secomind.com>

--- src/main.rs
+++ src/main.rs
@@ -11,6 +11,7 @@ use color_eyre::eyre::WrapErr;
 use std::time::SystemTime;
 use stream_rust_test::astarte::{send_data, ConnectionConfigBuilder, SdkConnection};
 use stream_rust_test::cli::Config;
+use stream_rust_test::shutdown::shutdown;
 use tokio::task::JoinSet;
 use tracing::{debug, error, info, warn};
 use tracing_subscriber::layer::SubscriberExt;
@@ -76,17 +77,30 @@ async fn main() -> eyre::Result<()> {
     tasks.spawn(send_data(client, now, cli_cfg));
 
     // handle tasks termination
-    while let Some(res) = tasks.join_next().await {
-        match res {
-            Ok(Ok(())) => {}
-            Err(err) if err.is_cancelled() => {}
-            Err(err) => {
-                error!(error = %err, "Task panicked");
-                return Err(err.into());
-            }
-            Ok(Err(err)) => {
-                error!(error = %err, "Task returned an error");
-                return Err(err);
+    loop {
+        tokio::select! {
+            _ = shutdown()? => {
+                info!("CTRL C received, shutting down");
+                tasks.abort_all();
+                break;
+            },
+            opt = tasks.join_next() => {
+                let Some(res) = opt else {
+                    break;
+                };
+
+                match res {
+                        Ok(Ok(())) => {}
+                        Err(err) if err.is_cancelled() => {}
+                        Err(err) => {
+                            error!(error = %err, "Task panicked");
+                            return Err(err.into());
+                        }
+                        Ok(Err(err)) => {
+                            error!(error = %err, "Task returned an error");
+                            return Err(err);
+                        }
+                    }
             }
         }
     }
